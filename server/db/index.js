33ff9d3a15c27c33e5a7726811c6cf964734314a
Alia Abdulahi <aliaabdulahicodes@gmail.com>
2021-05-01 16:01:36 -0400

    ANOTHA one

--- server/db/index.js
+++ server/db/index.js
@@ -4,7 +4,7 @@ const db = require("./db");
 
 const User = require("./models/user");
 
-const Product = require("./models/Product");
+const Product = require("./models/product");
 
 //associations could go here!
 
